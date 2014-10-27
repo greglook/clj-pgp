2b8dc3234ee047ba902462b7230422c1b72e3acc
Greg Look <greg@greg-look.net>
2014-10-27 01:33:01 -0700

    Convert codec-test to midje.

--- test/mvxcvi/crypto/pgp/codec_test.clj
+++ test/mvxcvi/crypto/pgp/codec_test.clj
@@ -1,7 +1,7 @@
 (ns mvxcvi.crypto.pgp.codec-test
   (:require
     [byte-streams :refer [bytes=]]
-    [clojure.test :refer :all]
+    [midje.sweet :refer :all]
     [mvxcvi.crypto.pgp :as pgp]
     [mvxcvi.crypto.pgp.test-keys :as keys])
   (:import
@@ -13,18 +13,27 @@
   (pgp/get-public-key keys/pubring "923b1c1c4392318a"))
 
 
-(deftest public-key-encoding
+(facts "public-key binary encoding"
   (let [encoded-key (pgp/encode pubkey)
         decoded-key (pgp/decode-public-key encoded-key)]
-    (is (instance? PGPPublicKey decoded-key))
-    (is (bytes= encoded-key (pgp/encode decoded-key))))
+    (fact "key is decoded as a PGP public key"
+      decoded-key => (partial instance? PGPPublicKey))
+    (fact "encoded key is canonical"
+      (pgp/encode decoded-key) => (partial bytes= encoded-key))))
+
+
+(facts "public-key ascii encoding"
   (let [encoded-key (pgp/encode-ascii pubkey)
         decoded-key (pgp/decode-public-key encoded-key)]
-    (is (string? encoded-key))
-    (is (instance? PGPPublicKey decoded-key))
-    (is (= encoded-key (pgp/encode-ascii decoded-key)))))
+    (fact "key is encoded as a string"
+      encoded-key => string?)
+    (fact "key is decoded as a PGP public key"
+      decoded-key => (partial instance? PGPPublicKey))
+    (fact "encoded key is canonical"
+      (pgp/encode-ascii decoded-key) => encoded-key)))
 
 
-(deftest signature-encoding
-  (is (thrown? IllegalArgumentException
-               (pgp/decode-signature (pgp/encode pubkey)))))
+(facts "signature encoding"
+  (fact "decoding non-signature value throws an exception"
+    (pgp/decode-signature (pgp/encode pubkey))
+    => (throws IllegalArgumentException)))
