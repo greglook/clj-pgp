19a368295631a474dc6f3f215f5a558f6db094eb
Greg Look <greg@greg-look.net>
2014-10-31 17:58:04 -0700

    Update project with plugins and aliases.

--- project.clj
+++ project.clj
@@ -6,12 +6,24 @@
 
   :deploy-branches ["master"]
 
+  :aliases {"docs" ["do" ["doc"] ["marg" "--multi" "--dir" "target/doc/marginalia"] ["hiera"]]
+            "tests" ["do" ["check"] ["test"] ["cloverage"]]}
+
+  :plugins [[codox "0.8.10"]
+            [lein-cloverage "1.0.2"]
+            [lein-marginalia "0.8.0"]]
+
   :dependencies [[byte-streams "0.1.13"]
                  [potemkin "0.3.11"]
                  [org.bouncycastle/bcpg-jdk15on "1.51"]]
 
-  :hiera {:cluster-depth 3}
+  :hiera {:path "target/doc/ns-hiera.png"
+          :cluster-depth 3}
 
-  :profiles {:dev {:plugins [[lein-cloverage "1.0.2"]]
-                   :dependencies [[midje "1.6.3"]
-                                  [org.clojure/clojure "1.6.0"]]}})
+  :codox {:defaults {:doc/format :markdown}
+          :output-dir "target/doc/api"
+          :src-dir-uri "https://github.com/greglook/clj-pgp/blob/master/"
+          :src-linenum-anchor-prefix "L"}
+
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :test {:dependencies [[midje "1.6.3"]]}})
