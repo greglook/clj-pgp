bb24a844fee1ef386d1401b831880509135dc432
Greg Look <greg@greg-look.net>
2014-11-22 11:54:01 -0800

    Require project namespaces in user ns.

--- dev/user.clj
+++ dev/user.clj
@@ -6,7 +6,8 @@
     [clojure.stacktrace :refer [print-cause-trace]]
     [clojure.string :as str]
     [clojure.tools.namespace.repl :refer [refresh]]
-    ; TODO: require local stuff
-    ))
+    [mvxcvi.crypto.pgp :as pgp]
+    (mvxcvi.crypto.pgp
+      [tags :as tags])))
 
-
+; ...
