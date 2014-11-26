92c87b6589f1bc7c7479dddabdd92765f86343ad
Greg Look <greg@greg-look.net>
2014-11-25 19:00:26 -0800

    Fix spacing in keyring ns.

--- src/mvxcvi/crypto/pgp/keyring.clj
+++ src/mvxcvi/crypto/pgp/keyring.clj
@@ -29,54 +29,70 @@
 
 
 (extend-protocol KeyRing
+
   PGPPublicKeyRing
 
-  (list-public-keys [this]
+  (list-public-keys
+    [this]
     (->> this .getPublicKeys iterator-seq))
 
-  (get-public-key [this id]
+  (get-public-key
+    [this id]
     (.getPublicKey this (key-id id)))
+
 
   PGPPublicKeyRingCollection
 
-  (list-public-keys [this]
+  (list-public-keys
+    [this]
     (->> this .getKeyRings iterator-seq (map list-public-keys) flatten))
 
-  (get-public-key [this id]
+  (get-public-key
+    [this id]
     (.getPublicKey this (key-id id)))
+
 
   PGPSecretKeyRing
 
-  (list-public-keys [this]
+  (list-public-keys
+    [this]
     (->> this .getPublicKeys iterator-seq))
 
-  (list-secret-keys [this]
+  (list-secret-keys
+    [this]
     (->> this .getSecretKeys iterator-seq))
 
-  (get-public-key [this id]
+  (get-public-key
+    [this id]
     (.getPublicKey this (key-id id)))
 
-  (get-secret-key [this id]
+  (get-secret-key
+    [this id]
     (.getSecretKey this (key-id id)))
+
 
   PGPSecretKeyRingCollection
 
-  (list-public-keys [this]
+  (list-public-keys
+    [this]
     (->> this .getKeyRings iterator-seq (map list-public-keys) flatten))
 
-  (list-secret-keys [this]
+  (list-secret-keys
+    [this]
     (->> this .getKeyRings iterator-seq (map list-secret-keys) flatten))
 
-  (get-public-key [this id]
+  (get-public-key
+    [this id]
     (let [id (key-id id)]
       (-> this (.getSecretKeyRing id) (.getPublicKey id))))
 
-  (get-secret-key [this id]
+  (get-secret-key
+    [this id]
     (.getSecretKey this (key-id id))))
 
 
 (defn load-public-keyring
-  "Loads a public keyring collection from a file."
+  "Loads a public keyring collection from a data source."
   [source]
   (with-open [stream (PGPUtil/getDecoderStream
                        (bytes/to-input-stream source))]
@@ -84,7 +100,7 @@
 
 
 (defn load-secret-keyring
-  "Loads a secret keyring collection from a file."
+  "Loads a secret keyring collection from a data source."
   [source]
   (with-open [stream (PGPUtil/getDecoderStream
                        (bytes/to-input-stream source))]
