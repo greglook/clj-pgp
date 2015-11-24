405c1e0050140757cb0a54a2efd19876a9488d3f
Greg Look <greg@greg-look.net>
2015-11-24 11:47:10 -0800

    Update dependency versions.

--- project.clj
+++ project.clj
@@ -9,11 +9,13 @@
   :aliases {"doc-lit" ["marg" "--multi" "--dir" "doc/marginalia"]
             "fuzz" ["with-profile" "+tool" "run" "-m" "clj-pgp.tool.fuzz"]}
 
-  :plugins [[lein-cloverage "1.0.2"]]
+  :plugins
+  [[lein-cloverage "1.0.2"]]
 
-  :dependencies [[byte-streams "0.2.0"]
-                 [org.bouncycastle/bcpg-jdk15on "1.52"]
-                 [org.bouncycastle/bcprov-jdk15on "1.52"]]
+  :dependencies
+  [[byte-streams "0.2.0"]
+   [org.bouncycastle/bcpg-jdk15on "1.53"]
+   [org.bouncycastle/bcprov-jdk15on "1.53"]]
 
   :hiera {:cluster-depth 1}
 
@@ -21,11 +23,11 @@
           :src-dir-uri "https://github.com/greglook/clj-pgp/blob/master/"}
 
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/clojure "1.6.0"]
-                                  [org.clojure/test.check "0.7.0"]
+                   :dependencies [[org.clojure/clojure "1.7.0"]
+                                  [org.clojure/test.check "0.9.0"]
                                   [org.clojure/tools.namespace "0.2.10"]]}
 
              :tool {:source-paths ["tool"]
-                    :dependencies [[mvxcvi/puget "0.8.1"]
-                                   [org.clojure/core.async "0.1.361.0-d8047c-alpha"]]
+                    :dependencies [[mvxcvi/puget "1.0.0"]
+                                   [org.clojure/core.async "0.2.374"]]
                     :jvm-opts []}})
