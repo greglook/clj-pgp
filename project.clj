d045f2cebd7a62e0e1b83f907bf82cb6282c5cc5
Greg Look <greg@mvxcvi.com>
2020-09-24 09:22:30 -0700

    Upgrade dependency and test versions.

--- project.clj
+++ project.clj
@@ -13,8 +13,8 @@
 
   :dependencies
   [[org.clojure/clojure "1.10.1" :scope "provided"]
-   [org.bouncycastle/bcpg-jdk15on "1.65"]
-   [org.bouncycastle/bcprov-jdk15on "1.65"]
+   [org.bouncycastle/bcpg-jdk15on "1.66"]
+   [org.bouncycastle/bcprov-jdk15on "1.66"]
    [byte-streams "0.2.4"]]
 
   :hiera
@@ -22,7 +22,7 @@
 
   :profiles
   {:dev
-   {:dependencies [[org.clojure/test.check "1.0.0"]]}
+   {:dependencies [[org.clojure/test.check "1.1.0"]]}
 
    :repl
    {:source-paths ["dev"]
@@ -33,5 +33,5 @@
 
    :tool
    {:source-paths ["tool"]
-    :dependencies [[mvxcvi/puget "1.2.1"]]
+    :dependencies [[mvxcvi/puget "1.3.1"]]
     :jvm-opts []}})
